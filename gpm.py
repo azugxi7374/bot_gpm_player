ee83c8949543b4e629850c190c6a13e2d9a92ee3
ixguza <rtk0253@gmail.com>
2017-10-11 01:25:03 +0900

    feature: gpmのsearch関連

--- gpm.py
+++ gpm.py
@@ -3,27 +3,66 @@
 from getpass import getpass
 import os
 from gmusicapi import Mobileclient
+import urllib.request
 
-USER_ENV = "PI1_GOOGLE_USER"
-PASS_ENV = "PI1_GOOGLE_PASS"
+USER_ENV = "GPM_GOOGLE_USER"
+PASS_ENV = "GPM_GOOGLE_PASS"
 
-user_name = os.getenv(USER_ENV)
-user_pass = os.getenv(PASS_ENV)
-if user_name is None: user_name = input("PI1_GOOGLE_USER: ")
-if user_pass is None: user_pass = getpass("PI1_GOOGLE_PASS: ")
+def fileDownload(url, fileName):
+    print("downloading ", url)
+    urllib.request.urlretrieve(url, fileName)
 
-api = Mobileclient()
-login_ok = api.login(user_name, user_pass, Mobileclient.FROM_MAC_ADDRESS)
-print("login: ", login_ok)
+def login(api):
+    while(not api.is_authenticated()):
+        user_name = os.getenv(USER_ENV)
+        user_pass = os.getenv(PASS_ENV)
+        if user_name is None: user_name = input(USER_ENV+": ")
+        if user_pass is None: user_pass = getpass(PASS_ENV+": ")
+    
+        api.login(user_name, user_pass, Mobileclient.FROM_MAC_ADDRESS)
+    return api
 
-plist = api.get_station_tracks('IFL')
+def findSongs(api, query):
+    tracks = [x["track"] for x in api.search(query)["song_hits"]]
 
-print(plist[0])
-print(api.get_stream_url(plist[0]['storeId']))
+def getPlaylist(api, query):
+    reslist = [plist["tracks"] for plist in api.get_all_user_playlist_contents() if query in plist["name"] if "tracks" in plist.keys()]
+    
+    if len(reslist) > 0:
+        return [x["track"] for x in reslist[0] if "track" in x.keys()]
+    else:
+        return []
 
-"""
-for t in plist:
-    url = api.get_stream_url(t['storeId'])
-    print(url)
-"""
-print("ok!")
+def getIFL(api):
+    tracks = api.get_station_tracks('IFL')
+    return tracks
+
+def run(api, query):
+    login(api)
+
+    tracks = []
+    if query == "":
+        # IFL
+        tracks = getIFL(api)
+    else:
+        tracks = getPlaylist(api, query)
+
+    for t in tracks:
+        print(disp(t))
+        # url = api.get_stream_url(t['storeId'])
+        # fileDownload(url, t["title"]+".mp3")
+    print("ok")
+
+
+def disp(track):
+    return track.get("artist", "_") + ": \"" + track.get("title", "_") + "\", " + track.get("album", "_") + " (" + str(track.get("year", "_")) + ")"
+
+
+
+if __name__ == "__main__":
+    api = Mobileclient()
+    while(True):
+        query = input("query: ")
+        run(api, query)
+
+
